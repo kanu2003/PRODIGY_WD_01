9ec753381e678c0f3a0f4a0d8b793976f5db9794
kanu2003 <139697832+kanu2003@users.noreply.github.com>
2024-04-08 15:43:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'block'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'none'
+}
